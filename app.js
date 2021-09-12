d8d99f4bb6ba1d3ba7d1914a45bb4c0f1ace3a30
Carson <rodriguescarson@hotmail.com>
2021-09-12 21:54:08 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var userName = prompt("give me a username");
+// var userName = prompt("give me a username");
 
-var welcomeMessage = 'Hello ' + userName;
+// var welcomeMessage = 'Hello ' + userName;
 
-alert(welcomeMessage);
+// alert(welcomeMessage);
+var btnTranslate = document.querySelector("#btn-translator");
+
+var txtInput = document.querySelector("#txt-Input");
+
+console.log(txtInput);
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+
+})
