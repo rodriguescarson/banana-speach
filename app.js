679060838b10f3c4744cfacfa90eede36c36bbe0
Carson <rodriguescarson@hotmail.com>
2021-09-13 10:37:51 +0530

    final

--- app.js
+++ app.js
@@ -6,7 +6,25 @@
 var btnTranslate = document.querySelector("#btn-translator");
 var outputDiv = document.querySelector("#output");
 var txtInput = document.querySelector("#txt-Input");
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrongwith server. try after some time")
+}
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    outputDiv.innerText = "adda tranlated " + txtInput.ariaValueMax;
+    // outputDiv.innerText = "adda tranlated " + txtInput.ariaValueMax;
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+          })
+    .catch(errorHandler)
 })
